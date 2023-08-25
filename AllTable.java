c8bba716f32af221326454b8f88f4c40efee0c3d
Jaydeep-0123 <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-25 13:23:02 +0530

    Add files via upload

---
+++ AllTable.java
@@ -0,0 +1,14 @@
+class Table
+{
+	public static void main(String[] args)
+	{
+		for(int i=2;i<=20;i++)
+		{
+			System.out.println(" ");
+			for(int j=1;j<=10;j++)
+			{
+				System.out.print(i*j+" ");
+			}
+		}
+	}
+}
