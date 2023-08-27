a9c09c8a3045f038bca5dd8e9a1d62ddf21376db
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-27 11:43:07 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,18 @@
+class Arithmetic
+{
+ 
+  public static void main(String[] args) {
+		
+		int a=20;
+		int b=30;
+		int sum=a+b;
+		int substriction=a-b;
+		int multiplaction=a*b;
+		int division=a/b;
+
+		System.out.println("the addition = "+(sum));
+		System.out.println("the substriction ="+(substriction));
+		System.out.println("the multiplaction ="+(multiplaction));
+		System.out.println("the division ="+(division));
+	}
+}
