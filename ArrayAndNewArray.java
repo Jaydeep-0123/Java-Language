152096d72b7fa41faaef5dc0564aa0adb2469870
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-08-28 09:27:24 +0530

    Add files via upload

---
+++ ArrayAndNewArray.java
@@ -0,0 +1,18 @@
+class NewArray
+{
+	public static void main(String[] args)
+	{
+		int a[]={1,2,3,4,5,6,7,8,9};
+
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		a=new int[5];
+		System.out.println();
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+}
