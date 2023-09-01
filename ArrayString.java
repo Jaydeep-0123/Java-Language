532e1da439426073a4375cf23d3da36466306529
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-09-01 20:08:17 +0530

    Add files via upload

---
+++ ArrayString.java
@@ -0,0 +1,16 @@
+class ArrayString
+{
+	public static void main(String[] args)
+	{
+		char ch[]={'t','h','i','s'};
+		char ph[]={'i','s'};
+		char gh[]={'t','e','s','t'};
+
+
+		String s=new String(ch);
+		String s1=new String(ph);
+		String s2=new String(gh);
+
+		System.out.println(s+s1+s2);
+	}
+}
