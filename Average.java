64cfc88bef443af533d1ab9b0bde232084231f67
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-09-03 13:48:51 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,14 @@
+class Average
+{
+	public static void main(String[] args)
+	{
+		int a=20;
+		int b=30;
+		int c=40;
+		int sum=a+b+c;
+		int ave=sum/3;
+
+		System.out.println("the sum of = "+sum);
+		System.out.println("the ave of = "+ave);
+	}
+}
