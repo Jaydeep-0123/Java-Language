5400c988a0b396b0fbadea6ec783958ce9701d8b
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-09-08 10:55:21 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+class EvenOdd
+{
+	public static void main(String[] args) {
+
+       System.out.print("Even");
+		for(int i=1;i<=100;i++)
+		{
+
+			if(i%2==0)
+            System.out.print(i+" ");
+        }
+
+         System.out.print("Odd");
+		for(int i=1;i<=100;i++)
+		{
+			if(i%2!=0)
+			System.out.print(i+" ");
+		}	
+	}
+}
