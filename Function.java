c6a44ed228f823dacade9818453e0abd7d7034ea
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-09-14 18:25:39 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,31 @@
+public class Function
+{
+	static void jaydeep()
+	{ 
+		System.out.println("i am jaydeep panwar");
+		System.out.println("i am from barahwah");
+		System.out.println("i am 20 years old");
+		System.out.println("i am doing BCA");
+		System.out.println();
+	}
+
+	static void kuldeep()
+	{
+		System.out.println("i am kuldeep panwar");
+		System.out.println("i am from barahwah");
+		System.out.println("i am 22 years old");
+		System.out.println("i am doing B.tech");
+		System.out.println();
+	}
+}
+class jay
+{
+	public static void main(String[] args)
+	{
+		Function.jaydeep();
+		Function.jaydeep();
+		Function.kuldeep();
+		Function.kuldeep();
+	}
+	
+}
