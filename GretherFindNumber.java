ae54b5e706d7d61ee0b9c7655d378c3308675f26
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-09-15 20:32:41 +0530

    Add files via upload

---
+++ GretherFindNumber.java
@@ -0,0 +1,12 @@
+ class GretherFindnumber
+{
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		if(a>b)
+	    System.out.printf("%d",a);
+	    else
+	    System.out.printf("%d",b);
+	}
+}
