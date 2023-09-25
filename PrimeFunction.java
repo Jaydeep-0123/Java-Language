ec5143f2944d13fbb8faf0b1d59f757ccd5c7e73
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-09-25 20:50:24 +0530

    Add files via upload

---
+++ PrimeFunction.java
@@ -0,0 +1,18 @@
+class Function
+{
+	public static void main(String[] args)
+	{
+		System.out.println(isprime(12));
+
+	}
+
+	static boolean isprime(int n)
+	{
+		for(int i=2;i<=n/2;i++)
+		{
+			if(n%i==0)
+				return false;
+		}
+		return true;
+	}
+}
