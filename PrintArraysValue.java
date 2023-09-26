41e41dc6549a25aa02891098d3bbffd2e68e0687
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-09-26 10:31:56 +0530

    Add files via upload

---
+++ PrintArraysValue.java
@@ -0,0 +1,12 @@
+class ArrayValue
+{
+	public static void main(String[] args)
+	{
+		int a[]={10,20,30,40,50};
+		int i;
+		for(i=0;i<a.length;i++)
+		{
+			System.out.println("the arrays value are = "+a[i]+" ");
+		}
+	}
+}
