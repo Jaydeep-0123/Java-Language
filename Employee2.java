f34d68c12620c8bbc1203a0056d83a777cad332d
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-10-12 15:48:49 +0530

    Add files via upload

---
+++ Employee2.java
@@ -0,0 +1,32 @@
+class Employee
+{
+    String name;
+    int salary;
+    String designation;   //data member and property and stance variable
+    String address;
+
+    Employee(String n , int s , String d , String a)   //constructor
+    {
+    	name=n;
+    	salary=s;
+    	designation=d;
+    	address=a;
+    }
+    void print()
+    {
+    	System.out.println("The employee name is = "+name);
+    	System.out.println("The employee salary is = "+salary);
+    	System.out.println("The employee designation is = "+designation);
+    	System.out.println("The epmloyee address is = "+address);
+        System.out.println();
+    }
+
+    public static void main(String[] args)
+    {
+    	Employee emp=new Employee("jaydeep" , 50000 , "maneger" , "indore");
+    	emp.print();
+
+        Employee emp1=new Employee("kuldeep" , 750000 , "doctor" , "ujjain");
+        emp1.print();
+    }
+}
