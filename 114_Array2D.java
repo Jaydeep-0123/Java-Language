e21095a0e71310e20705496ab5ae88fa3b2dd909
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-10-16 20:54:59 +0530

    Create 114_Array2D.java

---
+++ 114_Array2D.java
@@ -0,0 +1,23 @@
+class UserInputElement
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Rows :");
+		int rows=sc.nextInt();
+		System.out.println("Enter the Coloum :");
+		int coloum=sc.nextInt();
+
+		int arr[][]=new int[rows][coloum];
+
+        System.out.println("Enter the array element : ");
+		for(int i=0;i<rows;i++)
+		{
+			for(int j=0;j<coloum;j++)
+			{
+				arr[i][j]=sc.nextInt();
+			}
+		}
+
+		for(int i=0;i<rows;i++)
+		{
