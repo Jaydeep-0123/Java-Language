cf15bfce93b2a10cca71ee476e1530f551a2a710
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-10-16 20:48:57 +0530

    Update 110_1StringBuilder.java

--- 110_1StringBuilder.java
+++ 110_1StringBuilder.java
@@ -14,18 +14,3 @@ class Builder
 }
 
 
-//=========================================================================================
-
-class Build
-{
-	public static void main(String[] args) 
-	{
-		 StringBuilder sb=new StringBuilder("Tony");
-
-		 System.out.println(sb);
-		 System.out.println(sb.charAt(0));
-		 sb.setCharAT(0 ,'p');
-		 System.out.println(sb);
-
-	}
-}
