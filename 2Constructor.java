dd40e7267eb0acbed88c215450e4475faf0fa90a
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-10-17 16:26:16 +0530

    Add files via upload

---
+++ 2Constructor.java
@@ -0,0 +1,19 @@
+class Constructor
+{
+	Constructor()
+	{
+		System.out.println("This is a first Constructor");
+	}
+
+	Constructor()
+	{
+		System.out.println("This is a Second Constructor");
+	}
+}
+public class ConstructorSuper1
+{
+   public static void main(String[] args) 
+   {
+      Constructor s=new Constructor();	
+   }
+}
