c69358de088fe9523ec1943329cdbc7ee4fed039
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-10-18 15:53:50 +0530

    Update 117_ArrayList.java

--- 117_ArrayList.java
+++ 117_ArrayList.java
@@ -56,28 +56,3 @@ class StringArrayList
 
 
 
-//================================================================================================================
-
-
-class Array1
-{
-	public static void main(String[] args)
-	{
-		ArrayList<Integer> list=new ArrayList<Integer>();
-		list.add(10);
-		list.add(20);
-		list.add(30);
-		list.add(40);
-		list.add(50);
-		list.add(60);
-
-		System.out.println(list);
-
-		ArrayList<String> list1=new ArrayList<String>();
-		list1.add("Hello");
-		list1.add("Java");
-		list1.add("Programming");
-
-		System.out.println(list1);
-	}
-}		
