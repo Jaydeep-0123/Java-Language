26a9656bab29febd858045d00edda9e4cf6d8170
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-10-18 15:55:27 +0530

    Update 117_ArrayList.java

--- 117_ArrayList.java
+++ 117_ArrayList.java
@@ -29,30 +29,3 @@ class Array
 	}		
 }
 
-//============================================================================================================
-
-
-class StringArrayList
-{
-	public static void main(String[] args)
-	{
-		ArrayList<String> list=new ArrayList<String>();
-
-		list.add("jaydeep");
-		list.add("Kuldeep");
-		list.add("shivam");
-		list.add("rohan");
-
-		System.out.println(list);
-
-		String delete=list.remove(3);
-		System.out.println(delete);
-		System.out.println(list);
-
-		list.add(1,"surypal");
-		System.out.println(list);
-	}
-}
-
-
-
