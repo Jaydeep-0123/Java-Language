3b05a04987d1b8b57723ae50886118270a786ad2
Jaydeep Panwar <104197826+Jaydeep-0123@users.noreply.github.com>
2023-11-11 20:39:24 +0530

    Add files via upload

---
+++ 1StringBuilder.java
@@ -0,0 +1,31 @@
+class Builder
+{
+	public static void main(String[] args)
+	{
+		StringBuilder sb=new StringBuilder("h");
+		sb.append("e");
+		sb.append("l");
+		sb.append("l");
+		sb.append("o");
+
+
+		System.out.println(sb);
+	}
+}
+
+
+//=========================================================================================
+
+class Build
+{
+	public static void main(String[] args) 
+	{
+		 StringBuilder sb=new StringBuilder("Tony");
+
+		 System.out.println(sb);
+		 System.out.println(sb.charAt(0));
+		 sb.setCharAT(0 ,'p');
+		 System.out.println(sb);
+
+	}
+}
